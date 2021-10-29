66af06667f3abad0ca1f8e5336405d83bd2eb049
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-10-29 11:13:19 -0700

    Working on 4th test in console. still WIP. reconfiguring js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,1 @@
-function beepBoop(number) {
-  const numbers = [1, 2, 3];
-  const boops = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-  let boopBeep = "";
-
-  for (let i = 0; i < numbers.length; i++) {
-    if (number === numbers[i]) {
-      boopBeep = boops[i]
-      return boopBeep;
-    }
-  } 
-}
-
+function beepBoop(number) 
