853f0e73c781c6329dc93752d9eaf7161955d569
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-10-29 11:49:18 -0700

    Still working on 4th test. WIP going to lunch.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-function beepBoop(number) 
+function beepBoop(number) {
+  let output = new Array();
+  for (let i = 0; i <= number; i++) {
+    if (i === 13) { 
+      output.push("Won't you be my neighbor?");
+  } else if (i === 21) {
+      output.push("Boop!");
+  } else if (i === 32) {
+    output.push("Won't you be my neighbor?")
+  }
+  return beepBoop + output.join("", "") + "";
+}
