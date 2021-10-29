be192425783a80e6e170bddcf57d4494181da1a0
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-10-29 13:26:59 -0700

    Got tests completed. goes up to 40.

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ Expected output: "Won't you be my neighbor?"
 Test: If user inputs 5 it will display (0, "Beep!", "Boop!", "Won't you be my neighbor?", 4, 5)
 Code: "5"
 Expected output: "0, "Beep!", "Boop!", "Won't you be my neighbor?", 4, 5"
+
+Test: If user inputs 13, 21, 32 it results in the correct names.
+Code: "12, 21, or 32"
+Expected Output: "Boop!", "Boop!", "Won't you be my neighbor"
+
+Test: Make program get all numbers working correctly up to 40. 
+Code: "10"
+Expected output: "0, Beep!, Boop!, Won't you be my neighbor?, 5, 6, 7, 8, 9, Beep!" etc... to 40.
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
+let number = 40;
+let output = beepBoop(number);
+
 function beepBoop(number) {
-  let output = new Array();
+  let output = [];
   for (let i = 0; i <= number; i++) {
     if (i === 13) { 
-      output.push("Won't you be my neighbor?");
+      output.push('Won\'t you be my neighbor?');
   } else if (i === 21) {
       output.push("Boop!");
   } else if (i === 32) {
-    output.push("Won't you be my neighbor?")
+      output.push('Won\'t you be my neighbor?');
+  } else if (i.toString().charAt(0) === "1") {
+      output.push('Beep!'); 
+  } else if (i.toString().charAt(0) === "2") {
+      output.push('Boop!');
+  } else if (i.toString().charAt(0) === "3") {
+    output.push('Won\'t you be my neighbor?');
+  } else {
+    output.push(i);
   }
-  return beepBoop + output.join("", "") + "";
+}
+  return '' + output.join('','') + '';
+
 }
