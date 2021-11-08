071f1d428a13be517fa878c711539280910cf7d0
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-08 11:14:44 -0800

    Fixed if-else format, removed global var's

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,21 @@
-/*let number = 40;
-let output = beepBoop(number);*/
-
 function beepBoop(number) {
   let output = [];
   for (let i = 0; i <= number; i++) {
     if (i === 13 || i === 23) { 
       output.push('Won\'t you be my neighbor?');
-  } else if (i === 21 || i === 12) {
-      output.push("Boop!");
-  } else if (i === 32) {
+    } else if (i === 21 || i === 12) {
+        output.push("Boop!");
+    } else if (i === 32) {
+        output.push('Won\'t you be my neighbor?');
+    } else if (i.toString().includes("1")) {
+        output.push('Beep!'); 
+    } else if (i.toString().includes("2")) {
+        output.push('Boop!');
+    } else if (i.toString().includes("3")) {
       output.push('Won\'t you be my neighbor?');
-  } else if (i.toString().includes("1")) {
-      output.push('Beep!'); 
-  } else if (i.toString().includes("2")) {
-      output.push('Boop!');
-  } else if (i.toString().includes("3")) {
-    output.push('Won\'t you be my neighbor?');
-  } else {
-    output.push(i);
-  }
+    } else {
+      output.push(i);
+    }
 }
   return ' ' + output.join(', ',', ') + ' ';
 
